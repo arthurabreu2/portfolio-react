28e5f43a275d0d324e96dc8825f168950ce469d5
Arthur Abreu <arthurabreu2000@gmail.com>
2025-01-22 14:52:09 -0300

    Created Navbar and basic introdution

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import logo_av from "../assets/vetor-arthur.svg"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+    return <nav className="mb-20 flex items-center justify-between py-6 bg-transparent">
+        <div className="flex items-center pl-4">
+            <img src={logo_av} alt="Logo Arthur" className="mx-2 h-32 transition-all duration-500 hover:scale-150" />
+        </div>
+        <div className="m-8 flex justify-center items-center gap-4 text-2xl pr-4 ">
+            <FaLinkedin className="shadow-lg transition-all duration-400 hover:scale-125" />
+            <FaGithub className="shadow-lg transition-all duration-400 hover:scale-125" />
+            <FaInstagram className="shadow-lg transition-all duration-400 hover:scale-125" />
+            <FaSquareXTwitter className="shadow-lg transition-all duration-400 hover:scale-125" />
+        </div>
+    </nav>
+}
+
+export default Navbar;
