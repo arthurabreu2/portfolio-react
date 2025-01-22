8f53743a488fcd9866a7b74c6b0ec0e905e34922
Arthur Abreu <arthurabreu2000@gmail.com>
2025-01-22 19:52:53 -0300

    Added some features and animations for the website design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,43 @@
 import { HERO_CONTENT } from "../constants"
 import profilePic from "../assets/profile_picture.jpeg"
+import { ReactTyped } from "react-typed"
+
 
 const Hero = () => {
     return <div className="border-b border-neutral-900 pb-4 lg:mb-35">
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-5xl">
-                        Arthur Vinicius Abreu</h1>
+                    <h1 className="pb-16 font-thin drop-shadow-lg tracking-tight lg:mt-16 lg:text-5xl transition-all duration-1000 hover:scale-105">
+                        Arthur V. Abreu</h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-600 to-purple-700 bg-clip-text 
                     text-3xl tracking-tight text-transparent">Senior Full-stack Software Engineer</span>
+                    <span className="bg-gradient-to-r from-pink-300 via-slate-600 to-purple-700 bg-clip-text 
+                    text-lg tracking-tight text-transparent">
+                        <ReactTyped
+                            strings={[
+                                "and Scientist",
+                                "and DevOps",
+                                "and Curious",
+                                "and Passionate about Challenges",
+                                "and Determined",
+                            ]}
+                            typeSpeed={100}
+                            backSpeed={100}
+                            loop
+
+                        />
+                    </span>
                     <p className="my-2 text-1xl py-3 
                     font-extralight tracking-tighter">{HERO_CONTENT}</p>
                 </div>
             </div>
             <div className="w-full lg:w-1/2 lg:p-8">
-                <div className="flex justify-center">
-                    <img className="rounded-lg" width="75%" src={profilePic} alt="Arthur Photo" />
+                <div className="flex justify-center group-hover:scale-105">
+                    <div className="relative group w-80 h-80 rounded-lg overflow-hidden drop-shadow-lg">
+                        <img src={profilePic} alt="Arthur Photo" className="object-cover transition-all duration-1000 group-hover:scale-105" />
+                        <div className="absolute top-0 left-[-150%] w-[100%] h-full bg-gradient-to-r from-transparent to-white -skew-x-12 pointer-events-none opacity-10 group-hover:animate-shine" />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { FaInstagram } from "react-icons/fa6";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
-    return <nav className="mb-20 flex items-center justify-between py-6 bg-transparent">
-        <div className="flex items-center pl-4">
-            <img src={logo_av} alt="Logo Arthur" className="mx-2 h-32 transition-all duration-500 hover:scale-150" />
+    return <nav className="mb-10 flex items-center justify-between py-6 bg-transparent">
+        <div className="flex items-center">
+            <img src={logo_av} alt="Logo Arthur" className="mx-2 h-24 transition-all duration-500 hover:scale-150" />
         </div>
         <div className="m-8 flex justify-center items-center gap-4 text-2xl pr-4 ">
             <FaLinkedin className="shadow-lg transition-all duration-400 hover:scale-125" />
